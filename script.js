9568ff46674f53aa480980153a65ecc744181b67
Yuvan Vishnu Pandi <71508816+yuvanvishnupandi@users.noreply.github.com>
2025-02-15 09:49:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function appendValue(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculateResult() {
+    try {
+        document.getElementById("display").value = eval(document.getElementById("display").value);
+    } catch {
+        document.getElementById("display").value = "Error";
+    }
+}
